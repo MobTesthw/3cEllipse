47b63305ad30d63d53abfd28276d3d20bb1e85fa
mobtesthw <mobtesthw@gmail.com>
2019-12-23 22:11:54 +0200

    Initial commit

---
+++ src/sample/Controller.kt
@@ -0,0 +1,128 @@
+package sample
+
+import javafx.beans.value.ChangeListener
+import javafx.fxml.FXML
+import javafx.scene.Group
+import javafx.scene.control.Slider
+import javafx.scene.control.SplitPane
+import javafx.scene.control.TextArea
+import javafx.scene.layout.Pane
+import javafx.scene.paint.Color
+import javafx.scene.shape.Circle
+import javafx.scene.shape.Line
+import javafx.scene.shape.Rectangle
+import kotlin.math.abs
+import kotlin.math.sqrt
+
+
+class Controller{
+    val delta=2.0
+    var radius = 130.0
+    var radMultiplier=1.0
+    var radiusDistance=4.0
+
+    @FXML lateinit var viewportPane:Pane
+    @FXML lateinit var ta:TextArea
+    @FXML lateinit var splitPane:SplitPane
+    @FXML lateinit var radiusDistanceSlider:Slider
+    @FXML lateinit var radMultiplierSlider:Slider
+
+   fun initialize() {
+//       viewportPane.heightProperty().addListener { observable, oldValue, newValue -> repaintViewport() }
+//       repaintViewport()
+       viewportPane.setOnMouseClicked {e->repaintViewport()  }
+       radiusDistanceSlider.valueProperty().addListener { observable, oldValue, newValue ->
+           radiusDistance = radiusDistanceSlider.value
+           repaintViewport()
+           ta.appendText("\nnew distance: ${radiusDistanceSlider.value} ")
+       }
+       radMultiplierSlider.valueProperty().addListener { observable, oldValue, newValue ->
+           radMultiplier = radMultiplierSlider.value
+           repaintViewport()
+           ta.appendText("\nnew radius mult: ${radMultiplierSlider.value} ")
+       }
+   }
+
+   fun repaintViewport()    {
+       ta.text=""
+
+        splitPane.setDividerPosition(0,.9)
+
+        viewportPane.children.clear()
+        val w=viewportPane.width
+        val h=viewportPane.height
+
+        val a=h/(0.1+radiusDistance)
+        val b=Math.sqrt(2.0)*a
+
+       radius=3* sqrt(b*b+a*a)/4
+       radius*=radMultiplier
+
+        val xl=w/2-b/2
+        val yl=h/2+a/2
+
+        val xr=w/2+b/2
+        val yr=h/2+a/2
+
+        val xt=w/2
+        val yt=h/2-a/2
+
+        val group= Group()
+//
+        val circleL = Circle(3.0, Color.RED)
+        circleL.relocate(xl, yl)
+
+        val circleR = Circle(3.0, Color.BLUE)
+        circleR.relocate(xr, yr)
+
+        val circleT = Circle(3.0, Color.YELLOW)
+        circleT.relocate(xt, yt)
+
+        val rectangle = Rectangle(100.0, 100.0, Color.RED)
+        rectangle.relocate(70.0, 70.0)
+
+
+
+        group.children.addAll(circleT,circleR,circleL)
+
+
+        ta.text="w= $w  h=$h  xl= $xl  yl= $yl "
+
+       ta.appendText("\nw = ${w.toInt()}  h= ${h.toInt()}")
+       var n=0
+//       var br=0
+
+       for (i in 0..w.toInt()) {
+
+           for (j in 0..h.toInt()) {
+               val rl= sqrt((i-xl)*(i-xl)+(j-yl)*(j-yl))
+               val rr= sqrt((i-xr)*(i-xr)+(j-yr)*(j-yr))
+               val rt= sqrt((i-xt)*(i-xt)+(j-yt)*(j-yt))
+
+
+
+               val rad= rl+rr+rt
+
+               n++
+//               br++
+//               if(br>100000)break
+               if(n>1000){
+                   ta.appendText("\nrad - radius  = ${rad - radius}")
+                   n=0
+               }
+
+//               ta.appendText("delta = ${rad-radius}")
+//               ta.appendText("\nrad = ${rad}")
+
+               if (abs(rad - radius) <= delta) {
+                   group.children.add(Line(i.toDouble(), j.toDouble(), i.toDouble() + 1.0, j.toDouble() + 1.0))
+//                   ta.appendText("i= $i  j= $j")
+               }
+           }
+       }
+       viewportPane.children.addAll(group)
+    }
+
+}
+
+
---
+++ src/sample/Main.java
@@ -0,0 +1,30 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        stage.setTitle("3 Center");
+        Scene scene =new Scene(root, 800, 600);
+        stage.setScene(scene);
+
+        stage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
