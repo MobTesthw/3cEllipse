db22f1d373645aa2d512b7468a0644cb756fa230
mobtesthw <mobtesthw@gmail.com>
2019-12-24 20:55:28 +0200

    Duplication added

--- src/sample/Controller.kt
+++ src/sample/Controller.kt
@@ -12,8 +12,11 @@ import javafx.scene.paint.Color
 import javafx.scene.shape.Circle
 import javafx.scene.shape.Line
 import javafx.scene.shape.Rectangle
+import javafx.scene.transform.Rotate
+import javafx.scene.transform.Transform
 import kotlin.math.abs
 import kotlin.math.roundToInt
+import kotlin.math.sign
 import kotlin.math.sqrt
 
 
@@ -31,10 +34,13 @@ class Controller{
     @FXML lateinit var radDistanceLabel: Label
     @FXML lateinit var radMultiplierLabel:Label
 
+
+
+
    fun initialize() {
-//       viewportPane.heightProperty().addListener { observable, oldValue, newValue -> repaintViewport() }
-//       repaintViewport()
-       viewportPane.setOnMouseClicked {e->repaintViewport()  }
+
+//       viewportPane.setOnMouseClicked {e->repaintViewport()  }
+
        radDistanceSlider.valueProperty().addListener { observable, oldValue, newValue ->
            radDistance = radDistanceSlider.value
            val v:Double=(radDistanceSlider.value*1000).roundToInt().toDouble()/1000
@@ -49,22 +55,43 @@ class Controller{
            repaintViewport()
            ta.appendText("\nnew radius mult: ${radMultiplierSlider.value} ")
        }
+       radDistanceSlider.setOnScroll { e -> radDistanceSlider.value+=radDistanceSlider.blockIncrement*e.deltaY }
+       radMultiplierSlider.setOnScroll { e -> radMultiplierSlider.value+=radMultiplierSlider.blockIncrement* sign(e.deltaY) }
+       splitPane.setDividerPosition(0,.9)
+
    }
 
-   fun repaintViewport()    {
+   private fun repaintViewport()    {
+
        ta.text=""
 
-        splitPane.setDividerPosition(0,.9)
+       viewportPane.children.clear()
 
-        viewportPane.children.clear()
+       viewportPane.children.addAll(drawEllipse())
+   }
+    @FXML private fun btnDuplicateClick(){
+        val w=viewportPane.width
+        val h=viewportPane.height
+        val gr1=Group()
+        gr1.children.addAll(drawEllipse())
+        gr1.transforms.add((Transform.rotate(40.0,w/2,h/2)))
+
+        val gr2=Group()
+        gr2.children.addAll(drawEllipse())
+        gr2.transforms.addAll(Transform.rotate(80.0,w/2,h/2))
+
+        viewportPane.children.addAll(gr1, gr2)
+    }
+    private fun drawEllipse():Group{
+
         val w=viewportPane.width
         val h=viewportPane.height
 
         val a=h/(0.00000001+radDistance)
         val b=2.0*a/ sqrt(3.0)
 
-       radius=3* sqrt(b*b+a*a)/4
-       radius*=radMultiplier
+        radius=3* sqrt(b*b+a*a)/4
+        radius*=radMultiplier
 
         val xl=w/2-b/2
         val yl=h/2+a/2
@@ -89,47 +116,35 @@ class Controller{
         val rectangle = Rectangle(100.0, 100.0, Color.RED)
         rectangle.relocate(70.0, 70.0)
 
-       val circleC = Circle(1.0, Color.CORAL)
-       circleC.relocate(w/2, h/2)
+        val circleC = Circle(1.0, Color.CORAL)
+        circleC.relocate(w/2, h/2)
 
         group.children.addAll(circleT,circleR,circleL)
 
 
-       ta.appendText("w= $w  h=$h  xl= $xl  yl= $yl ")
+        ta.appendText("w= $w  h=$h  xl= $xl  yl= $yl ")
 
-       ta.appendText("\nw = ${w.toInt()}  h= ${h.toInt()}")
-//       var n=0
-//       var br=0
-
-       for (i in 0..w.toInt()) {
-
-           for (j in 0..h.toInt()) {
-               val rl= sqrt((i-xl)*(i-xl)+(j-yl)*(j-yl))
-               val rr= sqrt((i-xr)*(i-xr)+(j-yr)*(j-yr))
-               val rt= sqrt((i-xt)*(i-xt)+(j-yt)*(j-yt))
+        ta.appendText("\nw = ${w.toInt()}  h= ${h.toInt()}")
 
 
+        for (i in 0..w.toInt()) {
 
-               val rad= rl+rr+rt
+            for (j in 0..h.toInt()) {
+                val rl= sqrt((i-xl)*(i-xl)+(j-yl)*(j-yl))
+                val rr= sqrt((i-xr)*(i-xr)+(j-yr)*(j-yr))
+                val rt= sqrt((i-xt)*(i-xt)+(j-yt)*(j-yt))
 
-//               n++
-//               br++
-//               if(br>100000)break
-//               if(n>1000){
-//                   ta.appendText("\nrad - radius  = ${rad - radius}")
-//                   n=0
-//               }
 
-//               ta.appendText("delta = ${rad-radius}")
-//               ta.appendText("\nrad = ${rad}")
 
-               if (abs(rad - radius) <= delta) {
-                   group.children.add(Line(i.toDouble(), j.toDouble(), i.toDouble(), j.toDouble() ))
-//                   ta.appendText("i= $i  j= $j")
-               }
-           }
-       }
-       viewportPane.children.addAll(group)
+                val rad= rl+rr+rt
+
+
+                if (abs(rad - radius) <= delta) {
+                    group.children.add(Line(i.toDouble(), j.toDouble(), i.toDouble(), j.toDouble() ))
+                }
+            }
+        }
+       return group
     }
 
 }
