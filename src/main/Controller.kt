0b504ed0f4a27fdc7ca845f734fd916238402a35
mobtesthw <mobtesthw@gmail.com>
2019-12-25 00:43:55 +0200

    Center correction 1

--- src/main/Controller.kt
+++ src/main/Controller.kt
@@ -1,6 +1,5 @@
-package sample
+package main
 
-import javafx.beans.value.ChangeListener
 import javafx.fxml.FXML
 import javafx.scene.Group
 import javafx.scene.control.Label
@@ -12,16 +11,17 @@ import javafx.scene.paint.Color
 import javafx.scene.shape.Circle
 import javafx.scene.shape.Line
 import javafx.scene.shape.Rectangle
-import javafx.scene.transform.Rotate
 import javafx.scene.transform.Transform
+import shape2D.ShapesGroup
 import kotlin.math.abs
 import kotlin.math.roundToInt
 import kotlin.math.sign
 import kotlin.math.sqrt
 
 
+
 class Controller{
-    val delta=0.5
+
     var radius = 130.0
     var radMultiplier=1.0
     var radDistance=4.0
@@ -33,8 +33,6 @@ class Controller{
     @FXML lateinit var radMultiplierSlider:Slider
     @FXML lateinit var radDistanceLabel: Label
     @FXML lateinit var radMultiplierLabel:Label
-
-
 
 
    fun initialize() {
@@ -62,89 +60,27 @@ class Controller{
    }
 
    private fun repaintViewport()    {
+       val w=viewportPane.width
+       val h=viewportPane.height
 
        ta.text=""
 
        viewportPane.children.clear()
 
-       viewportPane.children.addAll(drawEllipse())
+       viewportPane.children.addAll(ShapesGroup.drawEllipse(w,h,radDistance,radMultiplier),ShapesGroup.screenCross(w,h))
    }
     @FXML private fun btnDuplicateClick(){
         val w=viewportPane.width
         val h=viewportPane.height
         val gr1=Group()
-        gr1.children.addAll(drawEllipse())
+        gr1.children.addAll(ShapesGroup.drawEllipse(w,h,radDistance,radMultiplier),ShapesGroup.screenCross(w,h))
         gr1.transforms.add((Transform.rotate(40.0,w/2,h/2)))
 
         val gr2=Group()
-        gr2.children.addAll(drawEllipse())
+        gr2.children.addAll(ShapesGroup.drawEllipse(w,h,radDistance,radMultiplier),ShapesGroup.screenCross(w,h))
         gr2.transforms.addAll(Transform.rotate(80.0,w/2,h/2))
 
         viewportPane.children.addAll(gr1, gr2)
-    }
-    private fun drawEllipse():Group{
-
-        val w=viewportPane.width
-        val h=viewportPane.height
-
-        val a=h/(0.00000001+radDistance)
-        val b=2.0*a/ sqrt(3.0)
-
-        radius=3* sqrt(b*b+a*a)/4
-        radius*=radMultiplier
-
-        val xl=w/2-b/2
-        val yl=h/2+a/2
-
-        val xr=w/2+b/2
-        val yr=h/2+a/2
-
-        val xt=w/2
-        val yt=h/2-a/2
-
-        val group= Group()
-//
-        val circleL = Circle(3.0, Color.RED)
-        circleL.relocate(xl, yl)
-
-        val circleR = Circle(3.0, Color.BLUE)
-        circleR.relocate(xr, yr)
-
-        val circleT = Circle(3.0, Color.YELLOW)
-        circleT.relocate(xt, yt)
-
-        val rectangle = Rectangle(100.0, 100.0, Color.RED)
-        rectangle.relocate(70.0, 70.0)
-
-        val circleC = Circle(1.0, Color.CORAL)
-        circleC.relocate(w/2, h/2)
-
-        group.children.addAll(circleT,circleR,circleL)
-
-
-        ta.appendText("w= $w  h=$h  xl= $xl  yl= $yl ")
-
-        ta.appendText("\nw = ${w.toInt()}  h= ${h.toInt()}")
-
-
-        for (i in 0..w.toInt()) {
-
-            for (j in 0..h.toInt()) {
-                val rl= sqrt((i-xl)*(i-xl)+(j-yl)*(j-yl))
-                val rr= sqrt((i-xr)*(i-xr)+(j-yr)*(j-yr))
-                val rt= sqrt((i-xt)*(i-xt)+(j-yt)*(j-yt))
-
-
-
-                val rad= rl+rr+rt
-
-
-                if (abs(rad - radius) <= delta) {
-                    group.children.add(Line(i.toDouble(), j.toDouble(), i.toDouble(), j.toDouble() ))
-                }
-            }
-        }
-       return group
     }
 
 }
---
+++ src/shape2D/ShapesGroup.kt
@@ -0,0 +1,97 @@
+package shape2D
+
+import javafx.scene.Group
+import javafx.scene.paint.CycleMethod
+import javafx.scene.paint.LinearGradient
+import javafx.scene.paint.Stop
+import javafx.scene.shape.Circle
+import javafx.scene.shape.Line
+import java.awt.Color
+import kotlin.math.abs
+import kotlin.math.sqrt
+
+class ShapesGroup {
+    companion object{
+        fun screenCross(w:Double,h:Double, color: Color = Color.GREEN):Group{
+
+            val group=Group()
+
+            val lineHorizontal = Line(0.0,h/2,w,h/2)
+            lineHorizontal.stroke= convertColorAwtToJfx(color)
+
+            val lineVertical = Line(w/2,0.0,w/2,h)
+            lineVertical.stroke= convertColorAwtToJfx(color)
+
+            group.children.addAll( lineHorizontal , lineVertical   )
+
+            return group
+        }
+        fun drawEllipse(w:Double,h:Double,radDistance:Double, radMultiplier:Double=1.0,showRadius:Boolean=true):Group{
+            val delta=0.5 //threshold to define point to draw
+
+            val a=h/(0.00000001+radDistance) // a - triangle height a=a1+a2 top and bottom parts
+
+            val b=2.0*a/ sqrt(3.0)
+            val a2 = b/4 //lowest triangle height part
+            val a1=a-a2
+
+            var radius=3* sqrt(b*b+a*a) /4
+            radius*=radMultiplier
+
+            val xl=w/2-b/2
+            val yl=h/2+a2
+
+            val xr=w/2+b/2
+            val yr=h/2+a2
+
+            val xt=w/2
+            val yt=h/2-a1
+
+            val group= Group()
+//
+            if (showRadius){
+                val circleL = Circle(3.0, javafx.scene.paint.Color.RED)
+                circleL.relocate(xl, yl)
+
+                val circleR = Circle(3.0, javafx.scene.paint.Color.BLUE)
+                circleR.relocate(xr, yr)
+
+                val circleT = Circle(3.0, javafx.scene.paint.Color.YELLOW)
+                circleT.relocate(xt, yt)
+
+                val circleC = Circle(1.0, javafx.scene.paint.Color.CORAL)
+                circleC.relocate(w/2, h/2)
+
+                group.children.addAll(circleT,circleR,circleL)
+            }
+
+            for (i in 0..w.toInt()) {
+
+                for (j in 0..h.toInt()) {
+                    val rl= sqrt((i-xl)*(i-xl)+(j-yl)*(j-yl))
+                    val rr= sqrt((i-xr)*(i-xr)+(j-yr)*(j-yr))
+                    val rt= sqrt((i-xt)*(i-xt)+(j-yt)*(j-yt))
+
+                    val rad= rl+rr+rt
+
+                    if (abs(rad - radius) <= delta) {
+                        group.children.add(Line(i.toDouble(), j.toDouble(), i.toDouble(), j.toDouble() ))
+                    }
+                }
+            }
+            return group
+        }
+    fun convertColorAwtToJfx(awtColor:Color):javafx.scene.paint.Color{
+//        java.awt.Color awtColor = ... ;
+        val r = awtColor.red
+        val g = awtColor.green
+        val b = awtColor.blue
+        val a = awtColor.alpha
+        val opacity = a / 255.0
+       return  javafx.scene.paint.Color.rgb(r, g, b, opacity)
+        /*Get each component from the awt Color object and use the javafx.scene.paint.Color.rgb(...) static method. Note that the awt Color has a getAlpha() method that returns the alpha as an int in the range 0-255, whereas javafx.scene.paint.Color.rgb(...) expects the alpha value as a double in the range 0.0-1.0:*/
+    }
+
+    }
+
+}
